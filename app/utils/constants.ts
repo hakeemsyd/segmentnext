59d1d26808ba612cfacce60c05ffcc2798340f3b
Hakeem Abbas <hakeem@Hakeems-MacBook-Pro.local>
2018-03-17 23:13:17 -0700

    Ejecting projects and taking sample code from gossip app which already built

---
+++ app/utils/constants.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+export class Constants {
+    private constructor() {
+    }
+
+    public static config = {
+        apiKey: "AIzaSyBbBG-ZsqaSj0yz_sOhXfV3J5SRH71SXfc",
+        authDomain: "gossip-bbb74.firebaseapp.com",
+        databaseURL: "https://gossip-bbb74.firebaseio.com",
+        projectId: "gossip-bbb74",
+        storageBucket: "gossip-bbb74.appspot.com",
+        messagingSenderId: "298354551253"
+    };
+
+    public static KEY_TOKEN = 'auth_token';
+}
